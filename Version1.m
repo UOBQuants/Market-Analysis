e17a61b8544df464d916927bde6f1ebd14f98a3a
Ashley Robertson <me@ashpace.co.uk>
2017-10-23 13:10:43 +0100

    Testing

--- Version1.m
+++ Version1.m
@@ -1,4 +1,14 @@
 Market = readtable('DB/Market_Data.csv');
-Compund = readtable('DB/Market_Data_CR.csv');
+Compound = readtable('DB/Market_Data_CR.csv');
 Return = readtable('DB/Market_Data_NR.csv');
+
+%https://uk.mathworks.com/help/matlab/matlab_prog/access-data-in-a-table.html
+%to learn how to access data 
+
+Companies = Market.Properties.VariableNames;
+[temp, size] = size(Companies);
+test = cell2mat(Companies(3));
+for i = 3:1:size
+    company = cell2mat(Companies(i));
+end
     
